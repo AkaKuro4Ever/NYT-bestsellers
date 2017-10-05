1ac57dcd040f68c97ce003aa1fe298724b84f63c
Deborah Seow <deborahseow9@gmail.com>
2017-10-05 15:06:48 +0000

    Tried to figure out input on #calll

--- lib/nyt_bestsellers/cli.rb
+++ lib/nyt_bestsellers/cli.rb
@@ -1,15 +1,21 @@
 class NytBestsellers::CLI
 
   def call
-    puts "Welcome to the New York Times Bestsellers List"
+    puts "Welcome to the New York Times Bestsellers List!"
     menu
     input = gets.strip
+
+    # puts input.upcase
+    if input == "All Books"
+      # || input == "All books" || input == "all books"
+      list_books
+    else
+      nil
+    end
   end
 
-  if input == "All Books" || "All books" || "all books"
-    list_books
-  end
-  
+
+
   def list_books
     #eventually will list all books
   end
@@ -19,5 +25,11 @@ class NytBestsellers::CLI
     puts "Author"
     puts "Genre"
     puts "All Books"
+  end
+
+  def book_description
+    puts "Please type the title of the book you'd like more information on."
+    input = gets.strip
+
   end
 end
