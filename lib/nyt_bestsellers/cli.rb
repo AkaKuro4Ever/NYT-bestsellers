1c01e98c5b96980a871eba7e465cdc2f712307e9
Deborah Seow <deborahseow9@gmail.com>
2017-10-05 15:11:47 +0000

    Figured out first gets.strip

--- lib/nyt_bestsellers/cli.rb
+++ lib/nyt_bestsellers/cli.rb
@@ -3,21 +3,14 @@ class NytBestsellers::CLI
   def call
     puts "Welcome to the New York Times Bestsellers List!"
     menu
-    input = gets.strip
-
     # puts input.upcase
-    if input == "All Books"
-      # || input == "All books" || input == "all books"
-      list_books
-    else
-      nil
-    end
   end
 
 
 
   def list_books
     #eventually will list all books
+    puts "hi"
   end
 
   def menu
@@ -25,6 +18,13 @@ class NytBestsellers::CLI
     puts "Author"
     puts "Genre"
     puts "All Books"
+    input = gets.strip
+    case input
+    when "All Books"
+    when "All books"
+    when "all books"
+      list_books
+    end
   end
 
   def book_description
